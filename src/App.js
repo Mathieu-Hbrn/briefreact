c396d527e9bc0429e7c519765ff4eb1bba14a660
mathieu.houbron <mathieu.houbron@campus-centre.fr>
2025-02-03 10:33:10 +0100

    victoire, ça marche! mais avec un css moisi!

--- src/App.js
+++ src/App.js
@@ -8,25 +8,31 @@ export default function App() {
     const [todos, setTodos] = useState([]);
     const [text, setText] = useState('');
 
+    {/*  Création d'une nouvelle tâche sur une base de tableau vide  */}
     function createTodos() {
         const newTodos = [];
         return newTodos;
     }
-
+    {/*  Supression d'une tâche par filtration d'id  */}
     function Supr(id) {
         setTodos(todos.filter((_item, index) => index !== id));
     }
 
     return (
-        <>
+        <div className="Todo">
+            {/*  Zone entrée de texte  */}
+            <h1>ToDo List</h1>
             <input
                 value={text}
                 onChange={e => setText(e.target.value)}
             />
+            {/*  Bouton d'ajout d'une tâche avec réinitialisation une fois clické  */}
             <button onClick={() => {
                 setTodos([...todos, text]);
                 setText('')
             }}>Add</button>
+
+            {/*  Affichage des tâches  */}
             <ul>
                 {todos.map((item, id) => (
                     <li key={id}>
@@ -34,6 +40,6 @@ export default function App() {
                     </li>
                 ))}
             </ul>
-        </>
+        </div>
     );
 }
